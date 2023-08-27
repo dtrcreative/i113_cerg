79a8b450fd2ce06a0ec833bfebda6479ab27c849
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-08-27 14:39:47 +0300

    Workflow without security

--- i113_eureka/src/main/java/com/micro/i113_eureka/I113EurekaApplication.java
+++ i113_eureka/src/main/java/com/micro/i113_eureka/I113EurekaApplication.java
@@ -3,8 +3,10 @@ package com.micro.i113_eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 @EnableEurekaServer
 public class I113EurekaApplication {
 
--- i113_gateway/src/main/java/com/micro/i113_gateway/I113GatewayApplication.java
+++ i113_gateway/src/main/java/com/micro/i113_gateway/I113GatewayApplication.java
@@ -2,12 +2,13 @@ package com.micro.i113_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class I113GatewayApplication {
-
+@CrossOrigin
+public class I113GatewayApplication{
     public static void main(String[] args) {
         SpringApplication.run(I113GatewayApplication.class, args);
     }
-
 }
