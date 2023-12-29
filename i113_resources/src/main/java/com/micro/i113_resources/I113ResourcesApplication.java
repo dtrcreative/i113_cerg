207bebf36d9560dfad383100de19e77a9a9cebc1
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-12-29 17:33:33 +0300

    Added: Resource module with simple structure

---
+++ i113_resources/src/main/java/com/micro/i113_resources/I113ResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.micro.i113_resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I113ResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I113ResourcesApplication.class, args);
+    }
+
+}
---
+++ i113_resources/src/main/java/com/micro/i113_resources/appconfig/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.micro.i113_resources.appconfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class ApplicationConfiguration {
+}
