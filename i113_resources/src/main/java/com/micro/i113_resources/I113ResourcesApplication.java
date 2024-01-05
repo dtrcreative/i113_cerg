09676bccc16ff2b9999bbdafbce5f5512fef08ca
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-01-05 14:51:00 +0300

    Added: ServletInitializer. OpenApiDefinition Fixed: FlyWayMigrations

--- i113_resources/src/main/java/com/micro/i113_resources/I113ResourcesApplication.java
+++ i113_resources/src/main/java/com/micro/i113_resources/I113ResourcesApplication.java
@@ -1,9 +1,12 @@
 package com.micro.i113_resources;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Resources"))
 public class I113ResourcesApplication {
 
     public static void main(String[] args) {
---
+++ i113_resources/src/main/java/com/micro/i113_resources/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.micro.i113_resources;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(I113ResourcesApplication.class);
+	}
+
+}
